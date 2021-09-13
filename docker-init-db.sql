bae3c851f6b9349e4bfc5b1218d139ef2791a41c
Dario Mory <dario@mory.dev>
2021-09-13 12:16:57 +0200

    :whale: Update docker files, integrate error templates with base

--- docker-init-db.sql
+++ docker-init-db.sql
@@ -1,4 +1,4 @@
-CREATE USER {{ project_name|lower }};
-CREATE DATABASE {{ project_name|lower }};
-GRANT ALL PRIVILEGES ON DATABASE {{ project_name|lower }} TO {{ project_name|lower }};
+CREATE USER sdg;
+CREATE DATABASE sdg;
+GRANT ALL PRIVILEGES ON DATABASE sdg TO sdg;
 
--- src/sdg/conf/docker.py
+++ src/sdg/conf/docker.py
@@ -10,6 +10,6 @@ os.environ.setdefault("LOG_STDOUT", "yes")
 
 # # Strongly suggested to not use this, but explicitly list the allowed hosts. It is
 # used to verify if a redirect is safe or not (open redirect vulnerabilities etc.)
-# os.environ.setdefault("ALLOWED_HOSTS", "*")
+os.environ.setdefault("ALLOWED_HOSTS", "*")
 
 from .production import *  # noqa isort:skip
