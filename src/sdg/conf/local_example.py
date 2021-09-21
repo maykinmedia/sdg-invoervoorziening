df748e97ed6dc1da6f1cb0c65dffa6606271cacc
Dax <dario@mory.dev>
2021-09-21 16:32:03 +0200

    :pencil2: Small adjustments to local conf

--- src/sdg/conf/local_example.py
+++ src/sdg/conf/local_example.py
@@ -3,8 +3,8 @@
 #
 import os
 
-os.environ.setdefault("TWO_FACTOR_PATCH_ADMIN", "no"),
-os.environ.setdefault("TWO_FACTOR_FORCE_OTP_ADMIN", "no"),
+os.environ.setdefault("TWO_FACTOR_PATCH_ADMIN", "no")
+os.environ.setdefault("TWO_FACTOR_FORCE_OTP_ADMIN", "no")
 
 
 DATABASES = {
@@ -13,7 +13,7 @@ DATABASES = {
         "NAME": "sdg",
         "USER": "sdg",
         "PASSWORD": "sdg",
-        "HOST": "",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        "PORT": "",  # Set to empty string for default.
+        "HOST": "localhost",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        "PORT": 5432,  # Set to empty string for default.
     }
 }
