ef8ebd8fbcce6b6fa7f350bcc176f6c6137b58f1
Dario Mory <dario@mory.dev>
2021-09-21 11:38:25 +0200

    :sparkles: Add admin formfield modification and filters

--- src/sdg/core/admin/catalogus.py
+++ src/sdg/core/admin/catalogus.py
@@ -3,14 +3,44 @@ from django.contrib import admin
 from sdg.core.models import ProductenCatalogus
 
 
+class IsReferenceCatalogFilter(admin.SimpleListFilter):
+    title = "Is Referentie"
+    parameter_name = "is_referentie_catalogus"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("Ja", "Ja"),
+            ("Nee", "Nee"),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "Ja":
+            return queryset.filter(is_referentie_catalogus=True)
+        elif value == "Nee":
+            return queryset.filter(is_referentie_catalogus=False)
+        return queryset
+
+
 @admin.register(ProductenCatalogus)
 class CatalogusAdmin(admin.ModelAdmin):
     model = ProductenCatalogus
 
     list_display = ("naam", "domein", "versie")
-    list_filter = ("domein", "naam")
+    list_filter = ("domein", "naam", IsReferenceCatalogFilter)
     ordering = ("domein", "naam")
     search_fields = (
         "naam",
         "domein",
     )
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        Zorg ervoor dat alleen referentiecatalogi kunnen worden geselecteerd.
+        """
+
+        if db_field.name == "referentie_catalogus":
+            kwargs["queryset"] = ProductenCatalogus.objects.filter(
+                is_referentie_catalogus=True
+            )
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
--- src/sdg/producten/admin/product.py
+++ src/sdg/producten/admin/product.py
@@ -11,6 +11,25 @@ from sdg.producten.models import (
 )
 
 
+class IsReferenceProductFilter(admin.SimpleListFilter):
+    title = "Is Referentie"
+    parameter_name = "referentie_product"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("Ja", "Ja"),
+            ("Nee", "Nee"),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "Ja":
+            return queryset.filter(referentie_product__isnull=True)
+        elif value == "Nee":
+            return queryset.filter(referentie_product__isnull=False)
+        return queryset
+
+
 class ProductGeneriekInformatieInline(admin.StackedInline):
     model = ProductGeneriekInformatie
     extra = 1
@@ -31,3 +50,13 @@ class GeneriekProductAdmin(MarkdownxModelAdmin):
 class ProductAdmin(MarkdownxModelAdmin):
     model = Product
     inlines = (ProductInformatieInline,)
+    list_filter = (IsReferenceProductFilter,)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        Ervoor zorgen dat alleen referentieproducten kunnen worden geselecteerd.
+        """
+
+        if db_field.name == "referentie_product":
+            kwargs["queryset"] = Product.objects.filter(referentie_product=None)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
