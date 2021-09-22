39a220e87c7f0e88ff9cdd831146bc1a9dd130ff
Dario Mory <dario@mory.dev>
2021-09-22 13:22:53 +0200

    :pencil2: Update logius admin

--- src/sdg/core/admin/logius.py
+++ src/sdg/core/admin/logius.py
@@ -11,18 +11,22 @@ from sdg.core.models import (
 @admin.register(Overheidsorganisatie)
 class OverheidsorganisatieAdmin(admin.ModelAdmin):
     search_fields = ("owms_pref_label",)
+    list_display = ("owms_pref_label", "owms_identifier", "owms_end_date")
 
 
 @admin.register(Informatiegebied)
 class InformatiegebiedAdmin(admin.ModelAdmin):
     search_fields = ("code", "informatiegebied")
+    list_display = ("code", "informatiegebied")
 
 
 @admin.register(Thema)
 class ThemaAdmin(admin.ModelAdmin):
     search_fields = ("thema",)
+    list_display = ("thema", "informatiegebied")
 
 
 @admin.register(UniformeProductnaam)
 class UniformeProductnaamAdmin(admin.ModelAdmin):
     search_fields = ("upn_label",)
+    list_display = ("upn_label", "thema")
