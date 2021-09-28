9881b19f64dc45910574df53f9abb57e7aa305ad
Dario Mory <dario@mory.dev>
2021-09-28 11:36:58 +0200

    :sparkles: Filter municipalities view based on roles -- #107

--- src/sdg/core/views.py
+++ src/sdg/core/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Subquery
 from django.views.generic import TemplateView
 
+from sdg.accounts.models import Role
 from sdg.organisaties.models import LokaleOverheid
 
 
-# TODO: Delete/modify (sample view)
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "core/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["lokale_overheden"] = LokaleOverheid.objects.all()
+        user = context["view"].request.user
+
+        roles = Role.objects.filter(user=user, is_redacteur=True)
+        context["lokale_overheden"] = LokaleOverheid.objects.filter(
+            pk__in=Subquery(roles.values("lokale_overheid")),
+        )
+
         return context
