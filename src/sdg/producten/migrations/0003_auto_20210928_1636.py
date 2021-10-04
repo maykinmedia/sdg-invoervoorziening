04eef82fa092a10fdef5a8205ab97fb7e8103fcb
Dario Mory <dario@mory.dev>
2021-10-04 16:25:50 +0200

    :card_file_box: Optimize migrations -- #18

--- src/sdg/producten/migrations/0003_auto_20210928_1636.py
+++ src/sdg/producten/migrations/0003_auto_20210928_1636.py
@@ -57,6 +57,8 @@ class Migration(migrations.Migration):
                     models.DateTimeField(
                         help_text="De datum van publicatie van de productversie.",
                         verbose_name="publicatie datum",
+                        blank=True,
+                        null=True,
                     ),
                 ),
                 (
@@ -92,9 +94,21 @@ class Migration(migrations.Migration):
                         on_delete=django.db.models.deletion.PROTECT,
                         related_name="versies",
                         to="producten.Product",
-                        verbose_name="referentie",
+                        verbose_name="product",
                     ),
                 ),
             ],
+        ),
+        migrations.AddField(
+            model_name="localizedproduct",
+            name="product_versie",
+            field=models.ForeignKey(
+                help_text="Het specifieke product van deze vertaling.",
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="vertalingen",
+                to="producten.ProductVersie",
+                verbose_name="product versie",
+                null=True,
+            ),
         ),
     ]
