6c2a0a00ef3312e974099729bd1e98fe6ec5e073
Dario Mory <dario@mory.dev>
2021-10-05 13:03:48 +0200

    :white_check_mark: Add tests for homepage

---
+++ src/sdg/core/tests/test_views.py
@@ -0,0 +1,48 @@
+from django.urls import reverse_lazy
+
+from django_webtest import WebTest
+
+from sdg.accounts.tests.factories import RoleFactory, UserFactory
+from sdg.core.tests.constants import Status
+
+HOME_URL = "core:home"
+
+
+class HomeViewTests(WebTest):
+    def setUp(self):
+        super().setUp()
+
+        self.user = UserFactory.create()
+        self.app.set_user(self.user)
+
+    def test_only_allowed_municipalities_are_displayed(self):
+        role1 = RoleFactory.create(user=self.user)
+        role2 = RoleFactory.create(user=self.user)
+        RoleFactory.create()
+        RoleFactory.create()
+
+        response = self.app.get(reverse_lazy("core:home"))
+        self.assertEqual(response.status_code, Status.OK)
+
+        municipalities = response.pyquery(".cards__card")
+
+        self.assertEqual(len(municipalities), 2)
+
+        self.assertEqual(
+            municipalities[0].text_content().strip(), str(role1.lokale_overheid)
+        )
+        self.assertEqual(
+            municipalities[1].text_content().strip(), str(role2.lokale_overheid)
+        )
+        self.assertEqual(
+            municipalities[0].attrib["href"],
+            role1.lokale_overheid.get_absolute_url(),
+        )
+
+    def test_real_name_is_displayed(self):
+        response = self.app.get(reverse_lazy("core:home"))
+        self.assertEqual(response.status_code, Status.OK)
+        self.assertIn(
+            self.user.get_full_name(),
+            response.text,
+        )
