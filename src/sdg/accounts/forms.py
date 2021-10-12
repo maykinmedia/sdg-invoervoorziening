46ce1ae178dbf1a7233b08addf2230c3ce53edbb
Dario Mory <dario@mory.dev>
2021-10-12 17:54:04 +0200

    :sparkles: Create forms for invitations -- #33

---
+++ src/sdg/accounts/forms.py
@@ -0,0 +1,36 @@
+from secrets import compare_digest
+
+from django import forms
+from django.forms import inlineformset_factory
+from django.utils.translation import ugettext_lazy as _
+
+from sdg.accounts.models import Role, User
+
+
+class RoleForm(forms.ModelForm):
+    class Meta:
+        model = Role
+        fields = (
+            "is_beheerder",
+            "is_redacteur",
+        )
+
+
+RoleInlineFormSet = inlineformset_factory(
+    User, Role, form=RoleForm, extra=1, max_num=1, can_delete=False
+)
+
+
+class InvitationAcceptForm(forms.Form):
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_confirm = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data["password"]
+        password_confirm = cleaned_data["password_confirm"]
+
+        if not compare_digest(password, password_confirm):
+            raise forms.ValidationError(
+                _("Wachtwoord en bevestigingswachtwoord komen niet overeen")
+            )
