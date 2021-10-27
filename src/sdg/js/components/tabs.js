4b32640ea83b26465a31588f2e80c8102283e00d
Dario Mory <dario@mory.dev>
2021-10-27 17:36:44 +0200

    :sparkles: Remember and select last tab language -- #190

--- src/sdg/js/components/tabs.js
+++ src/sdg/js/components/tabs.js
@@ -1,5 +1,17 @@
+import Cookies from 'js-cookie';
+
 const tabsTabs = document.querySelectorAll(".tabs__tab");
 const tabsContents = document.querySelectorAll(".tabs__tab-content");
+
+function toggleTab(target) {
+    [...tabsContents].forEach(tabsContent => tabsContent.classList.remove("tabs__tab-content--active"));
+    [...tabsTabs].forEach(tabsTab => tabsTab.classList.remove("tabs__tab--selected"));
+    target.classList.add("tabs__tab--selected");
+    const tabId = target.dataset.id;
+    const selectedContent = document.getElementById(tabId);
+    selectedContent.classList.add("tabs__tab-content--active");
+    Cookies.set("tab_lang", tabId)
+}
 
 class TabsToggle {
     constructor(node) {
@@ -7,13 +19,10 @@ class TabsToggle {
 
         this.node.addEventListener("click", (event) => {
             event.preventDefault();
-            [...tabsContents].forEach(tabsContent => tabsContent.classList.remove("tabs__tab-content--active"));
-            [...tabsTabs].forEach(tabsTab => tabsTab.classList.remove("tabs__tab--selected"));
-            event.currentTarget.classList.add("tabs__tab--selected");
-            const selectedContent = document.getElementById(event.currentTarget.dataset.id);
-            selectedContent.classList.add("tabs__tab-content--active");
+            toggleTab(event.currentTarget);
         });
     }
 }
 
 [...tabsTabs].forEach(tabsTab => new TabsToggle(tabsTab));
+
