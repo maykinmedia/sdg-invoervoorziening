fbf31aa0d1fcf1049a096668ff5ab320559ad5fd
Dario Mory <dario@mory.dev>
2021-10-29 14:52:13 +0200

    :white_check_mark: Add UPN catalog autofill tests -- #194

---
+++ src/sdg/core/tests/models/test_logius.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+from sdg.core.models import UniformeProductnaam
+from sdg.core.tests.factories.catalogus import ProductenCatalogusFactory
+from sdg.core.tests.factories.logius import UniformeProductnaamFactory
+
+
+class TestUniformeProductnaam(TestCase):
+    def test_save_upn_with_autofill_catalog(self):
+        autofill_catalog = ProductenCatalogusFactory.create(
+            autofill=True,
+            autofill_upn_filter=["SDG"],
+            is_referentie_catalogus=True,
+        )
+
+        upn = UniformeProductnaamFactory.create(upn_label="UPN1 (SDG)")
+        self.assertEqual(1, UniformeProductnaam.objects.count())
+        self.assertEqual(1, autofill_catalog.producten.count())
+
+        self.assertEqual(1, upn.generiek_producten.count())
+        generic_product = upn.generiek_producten.get()
+        self.assertEqual(2, generic_product.vertalingen.count())
+
+        self.assertEqual(1, generic_product.producten.count())
+        reference_product = generic_product.producten.get()
+
+        self.assertEqual(1, reference_product.versies.count())
+        reference_version = reference_product.versies.get()
+        self.assertEqual(2, reference_version.vertalingen.count())
+
+    def test_save_upn_without_autofill_catalog(self):
+        autofill_catalog = ProductenCatalogusFactory.create(
+            autofill=False,
+            is_referentie_catalogus=True,
+        )
+
+        UniformeProductnaamFactory.create(upn_label="UPN1 (SDG)")
+        self.assertEqual(1, UniformeProductnaam.objects.count())
+        self.assertEqual(0, autofill_catalog.producten.count())
