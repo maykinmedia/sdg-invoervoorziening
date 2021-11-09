700419bc7d278958526f7c5c21ed2a6674a09392
Dario Mory <dario@mory.dev>
2021-11-09 17:38:47 +0100

    :heavy_plus_sign: Add django-filter -- #27

--- src/sdg/conf/base.py
+++ src/sdg/conf/base.py
@@ -489,6 +489,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PARSER_CLASSES": ["rest_framework.parsers.JSONParser"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "PAGE_SIZE": 25,
 }
 SPECTACULAR_SETTINGS = {
