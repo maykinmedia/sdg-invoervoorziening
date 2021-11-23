637d616521d88d348da7bbfad68d0dafed049e80
Dario Mory <dario@mory.dev>
2021-11-23 12:14:39 +0100

    :ok_hand: Process PR feedback -- #204

--- src/sdg/js/components/tabs.js
+++ src/sdg/js/components/tabs.js
@@ -1,4 +1,4 @@
-const tabsTabs = document.querySelectorAll(".tabs__tab");
+const tabsTabs = document.querySelectorAll(".tabs--inline .tabs__tab");
 const tabsContents = document.querySelectorAll(".tabs__tab-content");
 
 class TabsToggle {
@@ -6,14 +6,12 @@ class TabsToggle {
         this.node = node;
 
         this.node.addEventListener("click", (event) => {
-            if (event.target.dataset.id) {
-                event.preventDefault();
-                [...tabsContents].forEach(tabsContent => tabsContent.classList.remove("tabs__tab-content--active"));
-                [...tabsTabs].forEach(tabsTab => tabsTab.classList.remove("tabs__tab--selected"));
-                event.currentTarget.classList.add("tabs__tab--selected");
-                const selectedContent = document.getElementById(event.currentTarget.dataset.id);
-                selectedContent.classList.add("tabs__tab-content--active");
-            }
+            event.preventDefault();
+            [...tabsContents].forEach(tabsContent => tabsContent.classList.remove("tabs__tab-content--active"));
+            [...tabsTabs].forEach(tabsTab => tabsTab.classList.remove("tabs__tab--selected"));
+            event.currentTarget.classList.add("tabs__tab--selected");
+            const selectedContent = document.getElementById(event.currentTarget.dataset.id);
+            selectedContent.classList.add("tabs__tab-content--active");
         });
     }
 }
