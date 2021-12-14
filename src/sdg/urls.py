1eea57f830965e8bf4ef12bfdffa82c722b0908b
Sjoerd Schipper <sjoerd@maykinmedia.nl>
2021-12-14 17:17:32 +0100

    add sdg to url paths

--- src/sdg/urls.py
+++ src/sdg/urls.py
@@ -20,50 +20,50 @@ admin.site.index_title = _("Welkom bij de SDG-admin")
 
 urlpatterns = [
     path(
-        "admin/password_reset/",
+        "sdg/admin/password_reset/",
         PasswordResetView.as_view(),
         name="admin_password_reset",
     ),
     path(
-        "admin/password_reset/done/",
+        "sdg/admin/password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
-    path("admin/hijack/", include("hijack.urls")),
-    path("admin/", admin.site.urls),
-    path("markdownx/", include("markdownx.urls")),
+    path("sdg/admin/hijack/", include("hijack.urls")),
+    path("sdg/admin/", admin.site.urls),
+    path("sdg/markdownx/", include("markdownx.urls")),
     path(
         "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
     path(
-        "reset/done/",
+        "sdg/reset/done/",
         auth_views.PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
     url(
-        r"^accept-invitation/(?P<key>\w+)/?$",
+        r"^sdg/accept-invitation/(?P<key>\w+)/?$",
         InvitationAcceptView.as_view(),
         name="invitation_accept",
     ),
-    path("api/", include("sdg.api.urls", namespace="api")),
-    path("accounts/", include("allauth.urls")),
-    path("accounts/", include("sdg.accounts.urls", namespace="accounts")),
-    path("organizations/", include("sdg.organisaties.urls", namespace="organisaties")),
-    path("", include("sdg.core.urls", namespace="core")),
-    path("", include(tf_urls)),
+    path("sdg/api/", include("sdg.api.urls", namespace="api")),
+    path("sdg/accounts/", include("allauth.urls")),
+    path("sdg/accounts/", include("sdg.accounts.urls", namespace="accounts")),
+    path("sdg/organizations/", include("sdg.organisaties.urls", namespace="organisaties")),
+    path("sdg/", include("sdg.core.urls", namespace="core")),
+    path("sdg/", include(tf_urls)),
 ]
 
 # NOTE: The staticfiles_urlpatterns also discovers static files (ie. no need to run collectstatic). Both the static
 # folder and the media folder are only served via Django if DEBUG = True.
 urlpatterns += staticfiles_urlpatterns() + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    "sdg"+settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
 
 if settings.DEBUG and apps.is_installed("debug_toolbar"):
     import debug_toolbar
 
     urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
+        path("sdg/__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
