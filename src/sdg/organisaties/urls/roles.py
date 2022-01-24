2c971fdbb72b335c911214a9c2088941b6946e54
Dario Mory <dario@mory.dev>
2022-01-24 15:46:53 +0100

    :children_crossing: Fix page titles and breadcrumb issues — #384

--- src/sdg/organisaties/urls/roles.py
+++ src/sdg/organisaties/urls/roles.py
@@ -1,11 +1,17 @@
 from django.conf.urls import url
 from django.urls import path
 
-from sdg.organisaties.views import RoleDeleteView, RoleListView, RoleUpdateView
+from sdg.organisaties.views import (
+    InvitationCreateView,
+    RoleDeleteView,
+    RoleListView,
+    RoleUpdateView,
+)
 
 app_name = "roles"
 urlpatterns = [
     path("", RoleListView.as_view(), name="list"),
+    path(r"invite/", InvitationCreateView.as_view(), name="invitation_create"),
     url(r"^(?P<role_pk>[\d]+)/update$", RoleUpdateView.as_view(), name="update"),
     url(r"^(?P<role_pk>[\d]+)/delete$", RoleDeleteView.as_view(), name="delete"),
 ]
