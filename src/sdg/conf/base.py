74703637fb0b0f6d9675ab39613ccf5a52054ad3
Joeri Bekker <joeri@maykinmedia.nl>
2022-02-04 18:01:59 +0100

    :bug: SUBPATH gets added already.

--- src/sdg/conf/base.py
+++ src/sdg/conf/base.py
@@ -545,9 +545,6 @@ if SUBPATH := config("SUBPATH", None):
     SUBPATH = f"/{SUBPATH.strip('/')}"
 
     if SUBPATH != "/":
-        STATIC_URL = f"{SUBPATH}{STATIC_URL}"
-        MEDIA_URL = f"{SUBPATH}{MEDIA_URL}"
-
         MARKDOWNX_URLS_PATH = f"{SUBPATH}/markdownx/markdownify/"
         MARKDOWNX_UPLOAD_URLS_PATH = f"{SUBPATH}/markdownx/upload/"
 
