d4b71280dbb38aa622f11316e989caa872503dbc
Dario Mory <dario@mory.dev>
2022-03-17 16:03:11 +0100

    :sparkles: Implement generic product updating — [#446]

--- src/sdg/services/management/commands/import_data_from_services.py
+++ src/sdg/services/management/commands/import_data_from_services.py
@@ -1,15 +1,36 @@
+from datetime import datetime
+
 from django.core.management import BaseCommand
 
-from sdg.producten.models import GeneriekProduct
+from sdg.producten.models import LocalizedGeneriekProduct
+from sdg.services.models import ServiceConfiguration
 
 
 class Command(BaseCommand):
     help = "Retrieve all configured APIs and fill generic products."
 
     def handle(self, **options):
-        from sdg.services.utils import retrieve_service_data
+        updated_objects = 0
 
-        # data = retrieve_service_data()
-        generic_products = []
-        # TODO: fill `generic_products` list
-        GeneriekProduct.objects.bulk_create(generic_products)
+        for service_config in ServiceConfiguration.objects.all():
+            products = service_config.retrieve_products()
+
+            for product in products:
+                localized_generic_products = LocalizedGeneriekProduct.objects.filter(
+                    generiek_product__doelgroep=service_config.doelgroep,
+                    generiek_product__upn__upn_label=product["upnLabel"],
+                    taal=product["taal"],
+                )
+                updated_objects += localized_generic_products.update(
+                    product_titel=product["titel"],
+                    generieke_tekst=product["tekst"],
+                    verwijzing_links=[list(i.values())[:2] for i in product["links"]],
+                    landelijke_link=product["url"],
+                    datum_check=datetime.fromisoformat(product["laatstGecheckt"]),
+                )
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully updated {updated_objects} localized generic products."
+            )
+        )
--- src/sdg/services/tests/test_management_commands.py
+++ src/sdg/services/tests/test_management_commands.py
@@ -1,11 +1,14 @@
 import json
 import os
+from datetime import datetime
 from unittest.mock import patch
 
 from zgw_consumers.models import Service
 
+from sdg.core.constants import DoelgroepChoices
+from sdg.core.tests.factories.logius import UniformeProductnaamFactory
 from sdg.core.tests.test_management_commands import CommandTestCase
-from sdg.producten.models import GeneriekProduct
+from sdg.producten.tests.factories.localized import LocalizedGeneriekProductFactory
 from sdg.services.models import ServiceConfiguration
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -13,16 +16,30 @@ TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class TestImportDataFromServices(CommandTestCase):
     def setUp(self):
-        service = Service.objects.create(
+        self.service = Service.objects.create(
             label="test",
             api_type="ztc",
-            api_root="https://example.com/api/v2/",
+            api_root="https://sdgapi.ondernemersplein.kvk.nl/api/v1/",
             auth_type="no_auth",
-            oas="https://example.com/api/v2/openapi.json",
+            oas="https://sdgapi.ondernemersplein.kvk.nl/swagger/v1/swagger.json",
         )
-        ServiceConfiguration.objects.create(
-            service=service,
-            doelgroep="eu-burger",
+        self.service_config = ServiceConfiguration.objects.create(
+            service=self.service,
+            doelgroep=DoelgroepChoices.burger,
+        )
+        self.upn_1 = UniformeProductnaamFactory.create(
+            upn_label="verklaring omtrent gedrag (vog)"
+        )
+        self.upn_2 = UniformeProductnaamFactory.create(upn_label="toeristenbelasting")
+        self.localized_1 = LocalizedGeneriekProductFactory.create(
+            generiek_product__upn=self.upn_1,
+            taal="en",
+            generiek_product__doelgroep=DoelgroepChoices.burger,
+        )
+        self.localized_2 = LocalizedGeneriekProductFactory.create(
+            generiek_product__upn=self.upn_2,
+            taal="en",
+            generiek_product__doelgroep=DoelgroepChoices.burger,
         )
 
     @patch("sdg.services.models.ServiceConfiguration.retrieve_products")
@@ -30,6 +47,51 @@ class TestImportDataFromServices(CommandTestCase):
         with open(os.path.join(TESTS_DIR, "data/example_api_response.json")) as f:
             retrieve_mock.return_value = json.load(f)["results"]
 
-        # out = self.call_command("import_data_from_services")
-        # TODO
-        self.assertEqual(20, GeneriekProduct.objects.count())
+        out = self.call_command("import_data_from_services")
+        self.assertIn("Successfully updated 2 localized generic products.", out)
+
+        self.localized_1.refresh_from_db()
+        self.assertEqual(
+            "Certificate of conduct for individuals (VOG NP)",
+            self.localized_1.product_titel,
+        )
+        self.assertIn("## VOG denial", self.localized_1.generieke_tekst)
+        self.assertEqual(
+            [
+                [
+                    "Certificate of conduct (Justis)",
+                    "https://www.justis.nl/en/products/certificate-of-conduct",
+                ],
+                ["VOG check tool (Justis, in Dutch)", "https://vogcheck.justis.nl/"],
+                [
+                    "Frequently asked questions certificate of conduct (Justis)",
+                    "https://www.justis.nl/en/service-contact/frequently-asked-questions/certificate-of-conduct",
+                ],
+                [
+                    "Certificate of conduct for legal entities (VOG RP)",
+                    "https://business.gov.nl/regulation/vogrp/",
+                ],
+                [
+                    "Applying for a permit or subsidy with eHerkenning",
+                    "https://business.gov.nl/regulation/applying-for-a-permit-or-subsidy-with-eherkenning/",
+                ],
+                [
+                    "Register for persons active in the childcare sector",
+                    "https://business.gov.nl/regulation/register-for-persons-active-in-the-childcare-sector/",
+                ],
+                [
+                    "Refusing certificate of conduct (VOG) based on police data",
+                    "https://business.gov.nl/amendment/refusing-certificate-conduct-vog-based-police-data/",
+                ],
+            ],
+            self.localized_1.verwijzing_links,
+        )
+        self.assertEqual(
+            "https://business.gov.nl/regulation/vog/", self.localized_1.landelijke_link
+        )
+        self.assertEqual(datetime(2021, 7, 28, 16, 27, 2), self.localized_1.datum_check)
+
+    @patch("zgw_consumers.client.ZGWClient.retrieve")
+    def test_import_data_from_services_unreachable_api(self, retrieve_mock):
+        retrieve_mock.side_effect = ConnectionError
+        self.call_command("import_data_from_services")
