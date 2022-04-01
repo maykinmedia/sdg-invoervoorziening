6f5ec09b3c9aefbd02bc5bf5a41c6ee00e2d7774
Joeri Bekker <joeri@maykinmedia.nl>
2022-04-01 16:17:17 +0200

    :lipstick: Named heading 3 and 4 appropriately

--- src/sdg/js/components/ckeditor.js
+++ src/sdg/js/components/ckeditor.js
@@ -36,8 +36,8 @@ ClassicEditor.defaultConfig = {
     heading: {
         options: [
             {model: 'paragraph', title: 'Paragraaf', class: 'ck-heading_paragraph'},
-            {model: 'heading1', view: 'h3', title: 'Kop 1', class: 'ck-heading_heading1'},
-            {model: 'heading2', view: 'h4', title: 'Kop 2', class: 'ck-heading_heading2'}
+            {model: 'heading2', view: 'h3', title: 'Kop 3', class: 'ck-heading_heading2'},
+            {model: 'heading3', view: 'h4', title: 'Kop 4', class: 'ck-heading_heading3'}
         ]
     },
     toolbar: {
