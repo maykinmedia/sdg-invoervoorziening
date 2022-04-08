6d044302eef485e2af52d3eb7f20491af95ebb20
Joeri Bekker <joeri@maykinmedia.nl>
2022-04-08 15:18:13 +0200

    :wrench: Add catalog update as task.

--- src/sdg/producten/tasks.py
+++ src/sdg/producten/tasks.py
@@ -7,3 +7,11 @@ from sdg.celery import app
 def autofill():
     """Automatically create generic and reference products."""
     call_command("autofill")
+
+
+@app.task()
+def update_catalogs():
+    """
+    Create, update, correct all specific products for each organisation.
+    """
+    call_command("update_catalogs")
