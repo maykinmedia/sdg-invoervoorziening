558a3439a3a04214d2707885c96fd0339f8f2acc
Joeri Bekker <joeri@maykinmedia.nl>
2022-04-09 10:50:27 +0200

    :bug: Fixed order of tasks.: UPN depends on IG

--- src/sdg/core/tasks.py
+++ src/sdg/core/tasks.py
@@ -7,8 +7,8 @@ from sdg.core.types import LoadCommand
 management_commands = [
     LoadCommand("load_government_orgs", PublicData.GOVERNMENT_ORGANISATION),
     LoadCommand("load_municipalities", PublicData.MUNICIPALITY),
-    LoadCommand("load_upn", PublicData.UPN),
     LoadCommand("load_informatiegebieden", PublicData.INFORMATIEGEBIED),
+    LoadCommand("load_upn", PublicData.UPN),
 ]
 
 
