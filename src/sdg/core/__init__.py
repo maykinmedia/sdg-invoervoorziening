e7e1bb24ef5099fdcdaba52b3d712ffad5b24a1f
Joeri Bekker <joeri@maykinmedia.nl>
2022-04-25 18:07:33 +0200

    :bug: Prevents an infinite loop when cached value is None.

--- src/sdg/core/__init__.py
+++ src/sdg/core/__init__.py
@@ -1,1 +1,11 @@
 default_app_config = "sdg.core.apps.CoreConfig"
+
+
+class empty:
+    """
+    This class is used to represent no data being provided for a given input
+    or output value.
+    It is required because `None` may be a valid input or output value.
+    """
+
+    pass
