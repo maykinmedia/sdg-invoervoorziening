cb3d1ce6697ee29d448dc0f96cf72ce67deee521
bart-maykin <bart.burgman@maykinmedia.nl>
2022-05-27 11:39:16 +0200

    :art: changed the way we grand permissions to in the api

--- src/sdg/api/models.py
+++ src/sdg/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from sdg.organisaties.models import LokaleOverheid
+
 
 class Token(models.Model):
     """Custom authorization token model without binding to a specific user."""
