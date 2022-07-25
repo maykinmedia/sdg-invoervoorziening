b3867ffc1c0b6fe00376ede30899c49ff36e019d
bart-maykin <bart.burgman@maykinmedia.nl>
2022-07-25 11:47:36 +0200

    :sparkles: [#651] added button to resend mail

---
+++ src/sdg/static/js/admin/resend_invite.js
@@ -0,0 +1,27 @@
+async function resendInvite(pk, subpath="") {
+    const url = `${window.location.origin}${subpath}/admin/resend_invite`
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+    const headers = {
+        'X-CSRFToken': csrftoken,
+        'Content-Type': 'application/json'
+    }
+
+    const body = {
+        pk
+    }
+
+    response = await fetch(url, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body),
+        mode: 'same-origin'
+    })
+        .then(response => response.status)
+
+    if (await response == 200) {
+        alert("De email is herstuurd");
+    } else {
+        alert("De uitnodiging is all geaccepteerd.");
+    }
+};
