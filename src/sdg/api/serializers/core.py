cd257edde761ecd69576243928912ef295d039dc
bart-maykin <bart.burgman@maykinmedia.nl>
2022-08-02 12:53:56 +0200

    :art: [#670] improved the api documentation texts

--- src/sdg/api/serializers/core.py
+++ src/sdg/api/serializers/core.py
@@ -11,7 +11,7 @@ class ProductenCatalogusSerializer(serializers.HyperlinkedModelSerializer):
     organisatie = LokaleOverheidBaseSerializer(source="lokale_overheid")
     producten = ProductBaseSerializer(
         many=True,
-        help_text="""Het product waar dit product van af hankelijk is dit geven we aan met een van de volgende velden:
+        help_text="""Het product waar dit product van afhankelijk is. Dit geven we aan met een van de volgende velden:
         - Fields: `url`, `upnUri`, `upnLabel`
         """,
     )
@@ -34,27 +34,27 @@ class ProductenCatalogusSerializer(serializers.HyperlinkedModelSerializer):
             "url": {
                 "view_name": "api:productencatalogus-detail",
                 "lookup_field": "uuid",
-                "help_text": "De url van de specifieke catalogi api call",
+                "help_text": "De URL van een specifieke productencatalogus, wordt tevens gebruikt om met andere aanroepen te refereren aan 1 catalogus.",
             },
             "uuid": {
-                "help_text": "De UUID(https://en.wikipedia.org/wiki/Universally_unique_identifier) van de specifieke catalogi"
+                "help_text": "De UUID(https://en.wikipedia.org/wiki/Universally_unique_identifier) van de specifieke catalogus"
             },
             "domein": {
                 "help_text": "Een afkorting die wordt gebruikt om het domein aan te duiden."
             },
-            "naam": {"help_text": "De naam van de catalogi"},
+            "naam": {"help_text": "De naam van de catalogus"},
             "is_referentie_catalogus": {
                 "help_text": "Dit is een boolean die aangeeft of de catalogus een referentie catalogus is, als deze op false staat dan is het een producten catalogus."
             },
             "referentie_catalogus": {
                 "lookup_field": "uuid",
                 "view_name": "api:productencatalogus-detail",
-                "help_text": "Dit is de api call url van de referentie catalogus waar dit product aan gekoppeld is.",
+                "help_text": "de URL van de referentieproductcatalogus waar deze productcatalogus aan gekoppeld is",
             },
             "toelichting": {
-                "help_text": "Hierin staat de toelichting beschreven waarom deze catalogi bestaat."
+                "help_text": "Hierin staat de toelichting beschreven waarom deze catalogus bestaat."
             },
             "versie": {
-                "help_text": "Dit is de versie van deze catalogi, hierdoor kunt u zien of er wijzigingen zijn aangebracht voor de catalogi."
+                "help_text": "Dit is de versie van deze catalogus, hierdoor kunt u zien of er wijzigingen zijn aangebracht voor de catalogus."
             },
         }
