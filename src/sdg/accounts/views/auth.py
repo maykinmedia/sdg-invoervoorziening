279a16e2ebf574845876a3758caccc2838413299
Dario Mory <dario@mory.dev>
2022-10-12 19:00:10 +0200

    :lock: Ensure django-axes is enabled and works with 2FA token — [vng-taiga#40]

--- src/sdg/accounts/views/auth.py
+++ src/sdg/accounts/views/auth.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import user_login_failed
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 from allauth.account.utils import get_next_redirect_url
 from allauth.account.views import RedirectAuthenticatedUserMixin
+from axes.decorators import axes_dispatch, axes_form_invalid
 from two_factor.views import LoginView as _LoginView
 
 from sdg.accounts.forms import AuthenticationForm
@@ -30,3 +33,31 @@ class LoginView(_LoginView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.form_list["auth"] = AuthenticationForm
+
+    @method_decorator(axes_dispatch)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    @method_decorator(axes_form_invalid)
+    def _form_invalid(self, *args, **kwargs):
+        """
+        Method called when a form is invalid (django-axes compat)
+        Ensure that the user_login_failed signal is sent when a token is invalid.
+        """
+        if self.request.POST.get("login_view-current_step") == "token":
+            user_login_failed.send(
+                sender=__name__,
+                credentials={
+                    "username": self.storage.authenticated_user.get_username(),
+                },
+                request=self.request,
+            )
+
+    def post(self, *args, **kwargs):
+        response = super().post(*args, **kwargs)
+        form = self.get_form(data=self.request.POST, files=self.request.FILES)
+
+        if not form.is_valid():
+            self._form_invalid(self)
+
+        return response
