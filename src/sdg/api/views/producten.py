de6f54b8623ca827609f5d88cbfaf653bbeb44ce
Dario Mory <dario@mory.dev>
2022-11-28 14:25:34 +0100

    :pencil2: Filter hidden products from viewset — [#845]

--- src/sdg/api/views/producten.py
+++ src/sdg/api/views/producten.py
@@ -161,6 +161,7 @@ class ProductViewSet(
     lookup_field = "uuid"
     queryset = (
         Product.objects.active_organization()
+        .filter(api_zichtbaarheid=True)
         .select_related(
             "catalogus",
             "catalogus__lokale_overheid",
