c53a1dea2edfd9c9471c8ac42360a9276b6afd81
bart-maykin <bart.burgman@maykinmedia.nl>
2023-01-31 17:53:26 +0100

    :sparkles: [#924] added js function to prevent submiting version form twice

---
+++ src/sdg/js/components/prevent_submit_twice.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('.form');
+
+class GenericForm {
+    /**
+     * Constructor method.
+     * @param {HTMLFormElement} node
+     */
+    constructor(node) {
+        /** @type {HTMLFormElement} */
+        this.node = node;
+
+        if(!this.isGenericForm()) {
+            return;  // Not a generic form.
+        }
+
+        if (this.node) {
+            this.disableButtons();
+        }
+    }
+
+    /**
+     * Returns whether `this.node` represents a generic form based on node tree.
+     * @return {boolean}
+     */
+    isGenericForm() {
+        return Boolean(this.getPublishButton(), this.getConceptButton());
+    }
+
+    /**
+     * Returns button (if found).
+     * @return {(Element|null)}
+    */
+    getPublishButton() {
+        return this.node.querySelector('[id$="publish-button"]');
+    }
+
+    /**
+     * Returns button (if found).
+     * @return {(Element|null)}
+    */
+    getConceptButton() {
+        return this.node.querySelector('[id$="concept-button"]');
+    }
+
+
+    disableButtons() {
+        const concept = this.getConceptButton()
+        const publish = this.getPublishButton()
+
+        if (concept && publish) {
+            let executed = false;
+
+            const disableFunc = (event) => {
+                if (executed) {
+                    event.preventDefault();
+                }
+                executed = true;
+            };
+
+            concept.addEventListener("click", disableFunc);
+            publish.addEventListener("click", disableFunc);
+        };
+    }
+
+}
+
+if (form) {
+    new GenericForm(form);
+}
