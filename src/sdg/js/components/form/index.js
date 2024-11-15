7ca2ba278308d532266a3d937434c29df36bc223
Stefan <stefan@maykinmedia.nl>
2024-11-15 11:25:31 +0100

    :lipstick: [#995] RENEW and REFACTOR update form and set the fields next to each other

--- src/sdg/js/components/form/index.js
+++ src/sdg/js/components/form/index.js
@@ -1,5 +1,5 @@
 import "./form_diff_button";
 import "./reference_diff_button";
-import "./language_switch";
 import "./show_reference_button";
 import "./use_reference_button";
+import "./utils";
--- src/sdg/js/components/generic.js
+++ src/sdg/js/components/generic.js
@@ -1,5 +1,5 @@
-import {availableEditors} from './markdown'
-const form = document.querySelector('.form');
+import { availableEditors } from "./markdown";
+const form = document.querySelector(".form");
 
 class GenericForm {
     /**
@@ -10,12 +10,12 @@ class GenericForm {
         /** @type {HTMLFormElement} */
         this.node = node;
         this.display = {
-            "valtOnder": false,
-            "aanwezig": false
-        }
+            valtOnder: false,
+            aanwezig: false,
+        };
 
-        if(!this.isGenericForm()) {
-            return;  // Not a generic form.
+        if (!this.isGenericForm()) {
+            return; // Not a generic form.
         }
 
         if (this.node) {
@@ -42,56 +42,57 @@ class GenericForm {
 
     collapseExpand() {
         const formSpecific = document.querySelector(".form__specific");
-        const formSpecificClassList = formSpecific.classList
+        const formSpecificClassList = formSpecific.classList;
 
         if (!this.display.valtOnder || !this.display.aanwezig) {
             formSpecific.style.pointerEvents = "none";
-            formSpecificClassList.add("tabs__table--hidden")
-            formSpecificClassList.add("form__specific--hidden")
+            formSpecificClassList.add("tabs__table--hidden");
+            formSpecificClassList.add("form__specific--hidden");
         }
 
-        if (this.display.valtOnder  && this.display.aanwezig) {
+        if (this.display.valtOnder && this.display.aanwezig) {
             formSpecific.style.pointerEvents = "all";
-            formSpecificClassList.remove("tabs__table--hidden")
-            formSpecificClassList.remove("form__specific--hidden")
+            formSpecificClassList.remove("tabs__table--hidden");
+            formSpecificClassList.remove("form__specific--hidden");
         }
     }
 
     displayHidden(dependency, field) {
         dependency.style.display = "none";
-        this.display[field] = true
-        this.collapseExpand()
+        this.display[field] = true;
+        this.collapseExpand();
     }
 
-    displayBlock(dependency, field) {
-        dependency.style.display = "block";
-        this.display[field] = false
-        this.collapseExpand()
+    displayGrid(dependency, field) {
+        dependency.style.display = "grid";
+        this.display[field] = false;
+        this.collapseExpand();
     }
 
     async emptyFieldValues(fields) {
-        if (await fields === []) {
+        fields = Array.isArray(fields) ? fields : [fields];
+        if (fields) {
             fields.forEach((item) => {
                 const textarea = item.querySelector("textarea");
                 textarea.value = "";
-            })
+            });
         }
     }
 
     async getProductTranslationName(productId, language) {
         // SubPath is a globally defined constant.
-        const url = `${window.location.origin}${SubPath}/cmsapi/translation/?product_id=${productId}&taal=${language}`
+        const url = `${window.location.origin}${SubPath}/cmsapi/translation/?product_id=${productId}&taal=${language}`;
         const translationJsonResults = await fetch(url)
-            .then(response => response.json())
-            .then(data => data.results[0])
+            .then((response) => response.json())
+            .then((data) => data.results[0]);
 
         if (translationJsonResults) {
             if (translationJsonResults.productTitel) {
-                return translationJsonResults.productTitel
+                return translationJsonResults.productTitel;
             }
-            return translationJsonResults.generiekProduct
+            return translationJsonResults.generiekProduct;
         }
-        return ""
+        return "";
     }
 
     resetSpecefiekeGegevens() {
@@ -101,73 +102,82 @@ class GenericForm {
             "[name=vertalingen-0-decentrale_procedure_link]",
             "[name=vertalingen-1-decentrale_procedure_link]",
             "[name=vertalingen-0-decentrale_procedure_label]",
-            "[name=vertalingen-1-decentrale_procedure_label]"
-        ]
+            "[name=vertalingen-1-decentrale_procedure_label]",
+        ];
         const array_fields = [
             "[name=vertalingen-0-verwijzing_links]",
             "[name=vertalingen-1-verwijzing_links]",
-        ]
-        const markdown_editor_fields = document.querySelectorAll('.markdownx textarea');
+        ];
+        const markdown_editor_fields = document.querySelectorAll(
+            ".markdownx textarea"
+        );
 
         none_markdown_editor_fields.forEach((field) => {
             let input_field = this.node.querySelector(field);
             if (input_field) {
-                input_field.value = ''
+                input_field.value = "";
             }
-        })
+        });
 
         array_fields.forEach((field) => {
             let input_fields = this.node.querySelectorAll(field);
             input_fields.forEach((input_field) => {
                 if (input_field) {
-                    input_field.value = ''
+                    input_field.value = "";
                 }
-            })
-        })
+            });
+        });
 
         markdown_editor_fields.forEach((field) => {
-            field.value = ''
-        })
+            field.value = "";
+        });
 
         Object.values(availableEditors).forEach((instance) => {
-            instance.editor.setData("")
-        })
+            instance.editor.setData("");
+        });
     }
 
     setUpDynamicProductAanwezig() {
         const input = this.node.querySelector("[name=product_aanwezig]");
-        const dependency = this.getClarificationField().closest(".form__language-wrapper");
+        const dependency = this.getClarificationField().closest(".form__field");
 
         if (input) {
             let previousIndex = input.selectedIndex;
 
             const displayFunc = async (displayDependency) => {
-                const controls = await displayDependency.querySelectorAll(".form__control")
+                const controls = await displayDependency.querySelectorAll(
+                    ".form__control"
+                );
                 if (input.selectedIndex === 2) {
                     if (input.selectedIndex != previousIndex) {
-                        if (confirm("Weet u zeker dat u dit product niet aanbiedt?\nAls u 'OK' antwoordt worden alle teksten leeggemaakt.")) {
+                        if (
+                            confirm(
+                                "Weet u zeker dat u dit product niet aanbiedt?\nAls u 'OK' antwoordt worden alle teksten leeggemaakt."
+                            )
+                        ) {
                             this.resetSpecefiekeGegevens();
-                            this.displayBlock(displayDependency, "aanwezig")
-                        }
-                        else {
+                            this.displayGrid(displayDependency, "aanwezig");
+                        } else {
                             input.selectedIndex = previousIndex;
-                            return
+                            return;
                         }
                     }
 
-                    controls.forEach(element => {
+                    controls.forEach((element) => {
                         const textarea = element.querySelector("textarea");
                         if (!textarea.value) {
-                            const defaultExplanation = document.querySelector(`.form__reference-${element.lang}`).dataset.productAanwezigToelichting;
+                            const defaultExplanation = document.querySelector(
+                                `.form__reference-${element.lang}`
+                            ).dataset.productAanwezigToelichting;
                             textarea.value = defaultExplanation;
                         }
                     });
                 } else {
-                    controls.forEach(async (fields) => {
+                    controls.forEach((fields) => {
                         this.displayHidden(dependency, "aanwezig");
                         this.emptyFieldValues(fields);
                     });
-                };
+                }
 
                 previousIndex = input.selectedIndex;
             };
@@ -176,56 +186,72 @@ class GenericForm {
             input.addEventListener("change", () => {
                 displayFunc(dependency);
             });
-        };
+        }
     }
 
     setUpDynamicProductValtOnder() {
         const select = document.querySelector("#id_product_valt_onder");
-        const dependency = document.querySelector('[id$="product_valt_onder_toelichting"]').closest(".form__language-wrapper");
+        const dependency = document
+            .querySelector('[id$="product_valt_onder_toelichting"]')
+            .closest(".form__field");
 
         if (select) {
             let previousSelectedProduct = select.value;
 
             const displayFunc = async (select, displayDependency) => {
                 if (select.selectedIndex > 0) {
-                    const controls = displayDependency.querySelectorAll(".form__control");
+                    const controls =
+                        displayDependency.querySelectorAll(".form__control");
 
                     if (previousSelectedProduct == null) {
-                        if (confirm("Weet u zeker dat dit product onder een ander product valt?\nAls u 'OK' antwoordt worden alle teksten leeggemaakt.")) {
+                        if (
+                            confirm(
+                                "Weet u zeker dat dit product onder een ander product valt?\nAls u 'OK' antwoordt worden alle teksten leeggemaakt."
+                            )
+                        ) {
                             this.resetSpecefiekeGegevens();
-                            this.displayBlock(displayDependency, "valtOnder")
+                            this.displayGrid(displayDependency, "valtOnder");
                         } else {
                             previousSelectedProduct = null;
                             select.selectedIndex = 0;
-                            return
+                            return;
                         }
                     }
 
                     for (const element of controls) {
                         previousSelectedProduct = select.value;
                         const textarea = element.querySelector("textarea");
-                        const defaultExplanation = document.querySelector(`.form__reference-${element.lang}`).dataset.defaultToelichting;
-                        const explanation = defaultExplanation.replace(/\[product\]/g, await this.getProductTranslationName(select.value, element.lang));
+                        const defaultExplanation = document.querySelector(
+                            `.form__reference-${element.lang}`
+                        ).dataset.defaultToelichting;
+                        const explanation = defaultExplanation.replace(
+                            /\[product\]/g,
+                            await this.getProductTranslationName(
+                                select.value,
+                                element.lang
+                            )
+                        );
                         textarea.value = explanation;
-                    };
+                    }
                 } else {
-                    const controls = displayDependency.querySelectorAll(".form__control")
-                    let index = 0
+                    const controls =
+                        displayDependency.querySelectorAll(".form__control");
+                    let index = 0;
                     for (const element of controls) {
                         const textarea = element.querySelector("textarea");
-                        this.displayHidden(dependency, "valtOnder")
-                        this.emptyFieldValues([controls[index], element])
-                        previousSelectedProduct = null
-                        index++
+                        this.displayHidden(dependency, "valtOnder");
+                        this.emptyFieldValues([controls[index], element]);
+                        previousSelectedProduct = null;
+                        index++;
                     }
-                };
+                }
             };
 
             displayFunc(select, dependency);
             select.addEventListener("change", (event) => {
                 displayFunc(event.target, dependency);
             });
-        };
+        }
     }
 }
 
