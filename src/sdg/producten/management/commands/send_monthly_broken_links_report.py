85d48ec5bb88679a2303682d53952c23c5e83e54
Stefan <stefan@maykinmedia.nl>
2024-11-26 12:22:32 +0100

    :recycle: [#993] REFACTOR some of the added code

--- src/sdg/producten/management/commands/send_monthly_broken_links_report.py
+++ src/sdg/producten/management/commands/send_monthly_broken_links_report.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
@@ -7,85 +9,44 @@ from django.utils.html import strip_tags
 
 from compat import render_to_string
 
+from sdg.accounts.models import Role
 from sdg.conf.utils import org_type_cfg
+from sdg.organisaties.models import LokaleOverheid
 from sdg.producten.models import BrokenLinks
 
 User = get_user_model()
 
 
-class BaseMail:
-    """
-    Description:
-    ------------
-    Abstract base mail implementation build for the send monthly broken links command.
-    This class is used to handle the email (create and send).
+class Command(BaseCommand):
+    help = "Send monthly broken links report to the all redactors of the content."
 
-    Parameters:
-    -----------
-    - user : User
-    - organization_id : int
-    - broken_link : BrokenLinks
-    """
-
-    def __init__(self, user, organization_id, broken_link, **kwargs):
-        self.user_full_name = user.get_full_name()
-        self.organizations = {organization_id}
-        self.broken_links = [broken_link]
-        self.receiver = user.email
-        self.sender_organization = org_type_cfg().organisation_name
-        self.ALLOW_MAILING = kwargs.get("allow_mailing", True)
-
-    def append_broken_link(self, broken_link, organization_id):
-        self.organizations.add(organization_id)
-        self.broken_links.append(broken_link)
-
-    def send_mail(self):
-        html_message = self.__create_email()
-        if self.ALLOW_MAILING:
-            send_mail(
-                "Rapportage foutieve links in SDG-invoervoorziening",
-                strip_tags(html_message),
-                settings.DEFAULT_FROM_EMAIL,
-                [self.receiver],
-                html_message=html_message,
+    def create_and_send_mail(self, user, broken_links, multiple_organizations: bool):
+        def sort_compare_fn(broken_link):
+            return (
+                broken_link.product.catalogus.lokale_overheid.__str__(),
+                broken_link.product.name,
+                broken_link.occuring_field,
             )
 
-    def __create_email(self):
         mail_context = {
-            "user_full_name": self.user_full_name,
-            "broken_links": self.broken_links,
-            "sender_organization": self.sender_organization,
-            "multiple_organizations": len(self.organizations) > 1,
+            "user_full_name": user.get_full_name(),
+            "broken_links": sorted(broken_links, key=sort_compare_fn),
+            "sender_organization": org_type_cfg().organisation_name,
+            "multiple_organizations": multiple_organizations,
         }
 
-        return render_to_string(
+        html_message = render_to_string(
             "producten/email/email_broken_links.html",
             context=mail_context,
         )
 
-
-class Command(BaseCommand):
-    help = "Send monthly broken links report to the all redactors of the content."
-
-    def create_or_append_receiver(
-        self,
-        user,
-        organisatie_id: str,
-        broken_link: BrokenLinks,
-        data: dict[str, BaseMail],
-    ):
-        base_mail = data.get(user.email)
-        if base_mail is not None:
-            base_mail.append_broken_link(
-                broken_link=broken_link, organization_id=organisatie_id
-            )
-        else:
-            data[user.email] = BaseMail(
-                allow_mailing=False,
-                user=user,
-                organization_id=organisatie_id,
-                broken_link=broken_link,
-            )
+        send_mail(
+            "Rapportage foutieve links in SDG-invoervoorziening",
+            strip_tags(html_message),
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
+            html_message=html_message,
+        )
 
     def handle(self, *args, **options):
         """
@@ -94,50 +55,43 @@ class Command(BaseCommand):
         Send an email with a report of the broken links to all roles with `ontvangt_mail = True` and `is_redacteur = True`. In case a lokale_overheid has only the roles
         `is_beheerder = True` and `is_redacteur = False` send the e-mail to the admins.
         """
-        mailing_dict: dict[str, BaseMail] = {}
-        notified_broken_links = BrokenLinks.objects.filter(error_count__gte=3)
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"There is a total of {len(notified_broken_links)} broken_links."
-            )
-        )
+
+        grouped_users = defaultdict(lambda: ([], set(), None))
 
         # First create mailing list
-        for broken_link in notified_broken_links:
-            roles = broken_link.product.catalogus.lokale_overheid.roles
-            organization_id = (
-                broken_link.product.catalogus.lokale_overheid.organisatie.id
+        for broken_link in BrokenLinks.objects.filter(error_count__gte=3):
+            lokale_overheid: LokaleOverheid = (
+                broken_link.product.catalogus.lokale_overheid
+            )
+            organization_id = lokale_overheid.organisatie_id
+            roles: Role = lokale_overheid.roles
+
+            receiver_roles: Role = (
+                # Receivers are every redactor with mailing on if there are redactors.
+                roles.filter(Q(is_redacteur=True, ontvangt_mail=True))
+                if roles.filter(Q(is_redacteur=True, ontvangt_mail=True)).count() >= 1
+                # When there are no redactors as receivers, send the e-mail to every admin with mailing on.
+                else roles.filter(Q(is_beheerder=True, ontvangt_mail=True))
             )
 
-            # Receiver (every redactor with ontvangt_email=True)
-            receiver_roles = roles.filter(Q(is_redacteur=True, ontvangt_mail=True))
-
-            if len(receiver_roles) == 0:
-                # Receiver (every admin with ontvangt_email=True)
-                receiver_roles = roles.filter(Q(is_beheerder=True, ontvangt_mail=True))
-
             for receiver_role in receiver_roles:
-                user = receiver_role.user
-                base_mail = mailing_dict.get(receiver_role.user.email)
-                if base_mail is not None:
-                    base_mail.append_broken_link(
-                        broken_link=broken_link, organization_id=organization_id
-                    )
-                else:
-                    mailing_dict[receiver_role.user.email] = BaseMail(
-                        allow_mailing=False,
-                        user=user,
-                        organization_id=organization_id,
-                        broken_link=broken_link,
-                    )
-
-        # Then send out emails
-        try:
-            for base_mail in mailing_dict.values():
-                base_mail.send_mail()
-        finally:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Successfully send emails to {len(mailing_dict.values())} user(s)"
+                broken_links, user_organizations, user = grouped_users[
+                    receiver_role.user.email
+                ]
+                user_organizations.add(organization_id)
+                broken_links.append(broken_link)
+                # Update user
+                grouped_users[receiver_role.user.email] = (
+                    broken_links,
+                    user_organizations,
+                    receiver_role.user,
                 )
+
+        # Send a mail to each user
+        for broken_links, user_organizations, user in grouped_users.values():
+            multiple_organizations = len(user_organizations) > 1
+            self.create_and_send_mail(
+                user=user,
+                broken_links=broken_links,
+                multiple_organizations=multiple_organizations,
             )
