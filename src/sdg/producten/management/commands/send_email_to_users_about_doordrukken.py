a333ce4899e9a28a0c25267e43f4b70c9dee6d18
Stefan <stefan@maykinmedia.nl>
2024-11-26 17:06:37 +0100

    :refactor: [#994] REFACTOR some of the logic and change the e-mail

--- src/sdg/producten/management/commands/send_email_to_users_about_doordrukken.py
+++ src/sdg/producten/management/commands/send_email_to_users_about_doordrukken.py
@@ -14,6 +14,11 @@ from furl import furl
 
 from sdg.conf.utils import org_type_cfg
 from sdg.producten.models import Product
+from sdg.organisaties.models import LokaleOverheid
+from sdg.core.models import ProductenCatalogus
+from sdg.accounts.models import Role
+
+from django.db.models.manager import BaseManager
 
 User = get_user_model()
 
@@ -21,19 +26,56 @@ User = get_user_model()
 class Command(BaseCommand):
     help = "Send an e-mail to users to inform them about their related products and that they will be overwritten."
 
-    def send_mail(self, user, product_url):
+    def construct_product_url(
+        self,
+        lokale_overheid: LokaleOverheid,
+        catalogus: ProductenCatalogus,
+        product: Product,
+    ) -> furl:
+        url_scheme = "https" if settings.IS_HTTPS else "http"
+        url_netloc = Site.objects.get_current().domain.rstrip("/")
+        url_partial_path = reverse(
+            "organisaties:catalogi:producten:edit",
+            kwargs={
+                "pk": lokale_overheid.pk,
+                "catalog_pk": catalogus.pk,
+                "product_pk": product.pk,
+            },
+        ).lstrip("/")
+
+        url_path = [segment for segment in [settings.SUBPATH, url_partial_path] if segment]
+
+        return furl(
+            scheme=url_scheme,
+            netloc=url_netloc,
+            path=url_path,
+        )
+
+    def create_and_send_mail(
+        self,
+        user,
+        product: Product,
+        catalogus: ProductenCatalogus,
+        lokale_overheid: LokaleOverheid,
+    ):
+        cfg = org_type_cfg()
+        product_url = self.construct_product_url(lokale_overheid, catalogus, product)
         mail_context = {
             "user_full_name": user.get_full_name(),
-            "sender_organization": org_type_cfg().organisation_name,
-            "product_url": product_url,
+            "sender_organization": cfg.organisation_name,
+            "product": product,
+            "product_url": product_url.url,
+            "product_url_with_action_query": product_url.add(
+                args={"doordrukken_action_taken": True}
+            ).url,
+            "org_type_name": cfg.name,
+            "lokale_overheid": lokale_overheid,
         }
 
         html_message = render_to_string(
             "producten/email/email_inform_about_doordrukken.html",
             context=mail_context,
         )
-
-        print(html_message)
 
         send_mail(
             "30 dagen tot een product in SDG automatisch zal worden gepubliceerd.",
@@ -44,41 +86,25 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        doordruk_datum = date.today() + timedelta(days=30)
-
+        # Get all specific products that will be pressed through in 30 days from now (not reference products).
         for product in Product.objects.filter(
             referentie_product__isnull=False,
             automatisch_doordrukken=True,
-            automatisch_doordrukken_datum=doordruk_datum,
+            automatisch_doordrukken_datum=date.today() + timedelta(days=30),
         ):
-            lokale_overheid = product.catalogus.lokale_overheid
+            catalogus: ProductenCatalogus = product.catalogus
+            lokale_overheid: LokaleOverheid = catalogus.lokale_overheid
+            roles: BaseManager[Role] = lokale_overheid.roles
 
-            url_scheme = "https" if settings.IS_HTTPS else "http"
-            url_netloc = Site.objects.get_current().domain.rstrip("/")
-            url_path_to_product = reverse(
-                "organisaties:catalogi:producten:edit",
-                kwargs={
-                    "pk": lokale_overheid.pk,
-                    "catalog_pk": product.catalogus.pk,
-                    "product_pk": product.pk,
-                },
-            ).lstrip("/")
-            url_path_list = [
-                segment
-                for segment in [settings.SUBPATH, url_path_to_product]
-                if segment
-            ]
-            product_url = furl(scheme=url_scheme, netloc=url_netloc, path=url_path_list)
-            product_url.args["doordrukken_action_taken"] = True
-
-            receiver_roles = lokale_overheid.roles.filter(
-                Q(is_redacteur=True, ontvangt_mail=True)
+            receiver_roles = (
+                # Receivers are every redactor with mailing on if there are redactors.
+                roles.filter(Q(is_redacteur=True, ontvangt_mail=True))
+                if roles.filter(Q(is_redacteur=True, ontvangt_mail=True)).count() >= 1
+                # When there are no redactors as receivers, send the e-mail to every admin with mailing on.
+                else roles.filter(Q(is_beheerder=True, ontvangt_mail=True))
             )
 
-            if len(receiver_roles) == 0:
-                receiver_roles = lokale_overheid.roles.filter(
-                    Q(is_beheerder=True, ontvangt_mail=True)
-                )
-
             for role in receiver_roles:
-                self.send_mail(role.user, product_url=product_url.url)
+                self.create_and_send_mail(
+                    role.user, product, catalogus, lokale_overheid
+                )
