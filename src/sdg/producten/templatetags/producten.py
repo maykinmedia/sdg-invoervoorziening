8a03d8d4b699717d12d4fc664e034a3d1f33327d
Stefan <stefan@maykinmedia.nl>
2024-11-27 16:25:16 +0100

    :art: [#994] RUN black and isort code formatters

--- src/sdg/producten/templatetags/producten.py
+++ src/sdg/producten/templatetags/producten.py
@@ -1,12 +1,10 @@
 from datetime import date, timedelta
+from typing import Literal
 
 from django import template
 
-from sdg.producten.types import ProductFieldMetadata
-
 from sdg.producten.models import Product
-
-from typing import Literal
+from sdg.producten.types import ProductFieldMetadata
 
 register = template.Library()
 
@@ -60,20 +58,22 @@ def doordruk_warning(product: Product):
         reference_product: Product = product.referentie_product
         ref_auto_forward = reference_product.automatisch_doordrukken
         ref_auto_forward_date = reference_product.automatisch_doordrukken_datum
-        original_publication_date = reference_product.automatisch_doordrukken_datum - timedelta(days=30)
+        original_publication_date = (
+            reference_product.automatisch_doordrukken_datum - timedelta(days=30)
+        )
         warning_in_timeframe = date.today() >= original_publication_date
 
         # When it isn't the right time yet, don't show the warning.
         if not warning_in_timeframe:
-            doordruk_activation_warning = None    
+            doordruk_activation_warning = None
         elif auto_forward and ref_auto_forward:
             doordruk_activation_warning = True
         elif not auto_forward and ref_auto_forward:
             doordruk_activation_warning = False
-        else: 
+        else:
             doordruk_activation_warning = None
 
     return {
         "doordruk_activation_warning": doordruk_activation_warning,
-        "datum__doordrukken": ref_auto_forward_date
+        "datum__doordrukken": ref_auto_forward_date,
     }
