612d708d355f3963501471e43bce7b7f4e657046
Joeri Bekker <joeri@maykinmedia.nl>
2025-04-01 21:12:20 +0200

    :bookmark: Bumped to version 1.9.3

--- src/sdg/__init__.py
+++ src/sdg/__init__.py
@@ -1,6 +1,6 @@
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
-__version__ = "1.9.2"
+__version__ = "1.9.3"
 __author__ = "Maykin"
 __homepage__ = "https://github.com/maykinmedia/sdg-invoervoorziening"
