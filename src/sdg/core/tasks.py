41e5151d0d0c5a8b76cc1d3b74925fea34d567b1
bart-maykin <bart.burgman@maykinmedia.nl>
2026-04-07 18:23:51 +0200

    :sparkles: [#1044] added cleanup task for the application reports

--- src/sdg/core/tasks.py
+++ src/sdg/core/tasks.py
@@ -3,6 +3,7 @@ from django.core.files import File
 
 from django.conf import settings
 from django.db import transaction
+from django.utils import timezone
 
 from sdg.celery import app
 from sdg.core.constants.logius import PublicData
@@ -48,3 +49,14 @@ def create_application_export(application_export_pk: str):
 
         export_model.file = File(file=export_file, name=name)
         export_model.save()
+
+
+@app.task()
+def clean_application_exports(days: int = 30):
+    """
+    Remove all rapports that were created over X days ago.
+    The default is set to 30 days.
+    """
+
+    remove_before = timezone.now().date() - timezone.timedelta(days=int(days))
+    ApplicationRapport.objects.filter(gemaakt_op__lt=remove_before).delete()
