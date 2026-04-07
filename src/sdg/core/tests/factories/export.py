cf71c67d1fa0bf4326e2d943156ea53cbdb5b3ff
bart-maykin <bart.burgman@maykinmedia.nl>
2026-04-07 18:26:04 +0200

    :sparkles: [#1044] updated factories - Added verantwoordelijke_organisaties post generation factory to the GeneriekProductFactory. - Created UserInvitationFactory to create UserInivatations. - Created ApplicationExportFactory.

---
+++ src/sdg/core/tests/factories/export.py
@@ -0,0 +1,9 @@
+from factory.django import DjangoModelFactory
+
+from ...models import ApplicationRapport
+
+
+class ApplicationExportFactory(DjangoModelFactory):
+
+    class Meta:
+        model = ApplicationRapport
