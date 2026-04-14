573a3ba12b861e3b1834b60d15ad7308b4abaa07
bart-maykin <bart.burgman@maykinmedia.nl>
2026-04-14 15:34:16 +0200

    :bento: updated celery beat generation file and updates the fixture files

--- bin/generate_celery_beat_fixture.sh
+++ bin/generate_celery_beat_fixture.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-src/manage.py dumpdata --indent=4 --natural-foreign --natural-primary django_celery_beat > src/sdg/fixtures/django_celery_beat.json
+src/manage.py dumpdata --indent=4 --natural-foreign --natural-primary --exclude=django_celery_beat.PeriodicTask django_celery_beat > src/sdg/fixtures/django_celery_beat.json
+src/manage.py dump_tasks
